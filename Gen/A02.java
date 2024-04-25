1dbbad703eb522fc5a8552437e9297a04f15510c
DrSkyRitik-15 <ritiksinghkushwaha06@gamil.com>
2024-04-25 23:06:46 +0530

    .

---
+++ Gen/A02.java
@@ -0,0 +1,47 @@
+class print<T> {
+    T a;
+
+    public void fun(T obj) {
+        a = obj;
+
+        System.out.println("The print single method => " + a);
+
+    }
+}
+
+/**
+ * A02
+ */
+public class A02 {
+    public static void main(String[] args) {
+        System.out.println("===============================");
+        ;
+        print<Integer> obj = new print<>();
+        obj.fun(10);
+        System.out.println("===============================");
+        
+
+        print<String> obj1 = new print<>();
+
+        obj1.fun("Hello");
+        System.out.println("===============================");
+        
+
+        print<Double> obj2 = new print<>();
+
+        obj2.fun(10.5);
+        System.out.println("===============================");
+        
+
+        print<Character> obj3 = new print<>();
+
+        obj3.fun('A');
+        System.out.println("===============================");
+        
+
+        print<Boolean> obj4 = new print<>();
+
+        obj4.fun(true);
+    }
+
+}
