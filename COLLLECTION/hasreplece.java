32314cd29b083f07e56d2f460b9b7545bf90e165
DrSkyRitik-15 <ritiksinghkushwaha06@gamil.com>
2024-04-26 12:40:47 +0530

    .

--- COLLLECTION/hasreplece.java
+++ COLLLECTION/hasreplece.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
+
 public class hasreplece {
     public static void main(String[] args) {
         Map<Integer, String> map = new HashMap<>();
@@ -13,7 +14,16 @@ public class hasreplece {
         System.out.println("Replace the value => ");
         map.replace(1, "AAaA");
         System.out.println(map);
+
+
         System.out.print("Size () => ");
         System.out.println(map.size());
+        
+        System.out.print("get() 1 => ");
+        System.out.println(map.get(1));
+
+        System.out.print("remove() => ");
+        System.out.println(map.remove(1));
+        System.out.println(map);
     }
 }
