068cdf1a945df431dd96259b5afd21a78992ade8
DrSkyRitik-15 <ritiksinghkushwaha06@gamil.com>
2024-04-26 12:28:24 +0530

    .

--- COLLLECTION/Hash.java
+++ COLLLECTION/Hash.java
@@ -4,9 +4,9 @@ public class Hash {
     public static void main(String[] args) {
         Map<Integer, String> map = new HashMap<>();
         map.put(1, "AA");
-        map.put(1, "BB");
-        map.put(1, "CC");
-        map.put(1, "DD");
+        map.put(2, "BB");
+        map.put(3, "CC");
+        map.put(4, "DD");
 
         System.out.println(map);
     }
